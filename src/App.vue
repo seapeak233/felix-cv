5bc86fe1153f60b27d29fe479c0da27505868385
seapeak <760945589@qq.com>
2024-06-18 17:03:07 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="徐建宇——测试网站" />
 </template>
 
 <style scoped>
